e674dda9f9f9ce2471c396db0a6751c3d2a71610
Jossaya Camille <jcamille2023@fau.edu>
2024-03-16 16:51:14 -0400

    Error corrections

--- public/js/about.js
+++ public/js/about.js
@@ -2,13 +2,19 @@ function show_member(name, description, url, role) {
     let element = document.getElementById("members");
     let basic = document.createElement("div");
     basic.classList.add('member');
-    basic.appendChild(document.createElement("h1").appendChild(document.createTextNode(name)));
-    basic.appendChild(document.createElement('p').appendChild(document.createTextNode(role)));
+    let h1 = document.createElement("h1");
+    let p_role = document.createElement("p");
+    h1.appendChild(document.createTextNode(name));
+    p_role.appendChild(document.createTextNode(role))
+    basic.appendChild(h1);
+    basic.appendChild(p_role);
     let img = document.createElement('img');
     img.src = url;
     basic.appendChild(img);
     let bio = document.createElement("div");
-    bio.appendChild(document.createElement("p").appendChild(document.createTextNode(description)));
+    let p_des = document.createElement("p");
+    p_des.appendChild(document.createTextNode(description))
+    bio.appendChild(p_des);
     element.appendChild(basic);
     element.appendChild(bio);
 }
