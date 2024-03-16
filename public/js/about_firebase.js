e8ec9a21a3badacde03c9ccbb88c4a48a137cfdc
Jossaya Camille <jcamille2023@fau.edu>
2024-03-16 16:34:32 -0400

    Error correction

--- public/js/about_firebase.js
+++ public/js/about_firebase.js
@@ -23,7 +23,7 @@ const analytics = getAnalytics(app);
 const database = getDatabase();
 const dbRef = ref(database);
 
-get(ref(database, "/members/"), (snapshot) => {
+get(child(dbRef, "/members/"), (snapshot) => {
   let data = snapshot.val();
   console.log(data);
   data = Object.values(data);
