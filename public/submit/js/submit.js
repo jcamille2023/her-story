8b5320d7c1f8c3def68018762b339c1f34a38bfe
Jossaya Camille <jcamille2023@fau.edu>
2024-03-31 18:09:36 -0400

    Bug fixes

--- public/submit/js/submit.js
+++ public/submit/js/submit.js
@@ -2,51 +2,156 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebas
 import { getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
 import { getDatabase, set, ref, onValue, get, child, push } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
 import {WebWrite} from 'https://jcamille2023.github.io/webwrite/webwrite_oop.js';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-var user;
-var uid;
-let post;
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-apiKey: "AIzaSyCjNvzBC-xVZ0tW14acmAhLLH-kjizRIG4",
-authDomain: "herstory-9124d.firebaseapp.com",
-projectId: "herstory-9124d",
-storageBucket: "herstory-9124d.appspot.com",
-messagingSenderId: "266596236872",
-appId: "1:266596236872:web:e80ff220069219a3bd627d",
-measurementId: "G-9WV20CMFLM"
+    apiKey: "AIzaSyCjNvzBC-xVZ0tW14acmAhLLH-kjizRIG4",
+    authDomain: "herstory-9124d.firebaseapp.com",
+    projectId: "herstory-9124d",
+    storageBucket: "herstory-9124d.appspot.com",
+    messagingSenderId: "266596236872",
+    appId: "1:266596236872:web:e80ff220069219a3bd627d",
+    measurementId: "G-9WV20CMFLM"
 };
 
-// Initialize Firebase
+   
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
 const dbRef = ref(database);
-let container = document.getElementById("input-container");
 let webwrite = new WebWrite();
 
+onAuthStateChanged(auth, (user) => {
+    if(user) {
+        landing_page();
+    }
+});
 
-function logout() {
-  signOut(auth).then(() => {
-  console.log("User is signed out.");
-  window.location.href = "https://herstory-9124d.firebaseapp.com";
-  }).catch((error) => {
-  // An error happened.
-  });
+function landing_page() {
+    let user = auth.currentUser;
+    let uid = user.uid;
+    set(ref(database, 'users/' + uid), {
+        name: user.displayName,
+        email: user.email,
+      });
+      document.getElementById("display_name").innerHTML = user.displayName;
+      // Basic text to submit a new post
+      document.getElementById("text-container").innerHTML = "<h1>Let's get started!</h1>";
+      document.getElementById("text-container").innerHTML += "<br><h2>Basic Info</h2>";
+      document.getElementById("text-container").innerHTML += "<br>";
+      document.getElementById("text-container").innerHTML += "<p>Let's confirm a few details about your post.</p>";
+      // Details
+      var name_text = document.createElement("p");
+      name_text.textContent = "Name: " + user.displayName;
+      document.getElementById('input-container').appendChild(name_text);
+      var email_text = document.createElement("p");
+      email_text.textContent = "Email: " + user.email;
+      document.getElementById('input-container').appendChild(email_text);
+      document.getElementById('input-container').innerHTML += "<p>Is this information valid?</p>";
+      document.getElementById('input-container').innerHTML += '<table><tr><td><button onclick="valid()">Yes</button></td><td><button onclick="invalid()">No</button></td></tr></table>';
+      document.getElementById('input-container').innerHTML += "<p>Press no only if the name on your Google account is not your actual name or there are multiple authors.</p>";
+      document.getElementById('input-container').innerHTML += "<br>";
+      document.getElementById('input-container').innerHTML += "<p>If your Google account name is not your real name, you can add your real name to HerStory by going to the settings page.</p>";
+      
 }
-window.logout = logout;
+
+function valid() {
+    let user = auth.currentUser;
+    let container = document.getElementById('input-container');
+    container.innerHTML = "";
+
+    let ele1 = document.createElement('p');
+    ele1.textContent = 'Title of post';
+    container.appendChild(ele1);
+
+    let input = document.createElement('input');
+    input.setAttribute('type','text');
+    container.appendChild(input);
+
+    let ele2 = document.createElement('p');
+    ele2.textContent = 'Write a brief description of your post.';
+    container.appendChild(ele2);
+
+
+    let description = document.createElement('textarea');
+    container.appendChild(description);
+
+    let ele3 = document.createElement('p');
+    ele3.textContent = 'Blog post or interview?';
+    container.appendChild(ele3);
+
+    let choice1 = document.createElement('input');
+    choice1.setAttribute('type','radio');
+    container.appendChild(choice1);
+    let label1 = document.createElement('label');
+    label1.textContent = 'Blog';
+    container.appendChild(label1);
+    
+
+    let choice2 = document.createElement('input');
+    choice2.setAttribute('type','radio');
+    container.appendChild(choice2);
+    let label2 = document.createElement('label');
+    label2.textContent = 'Interview';
+    container.appendChild(label2);
+
+    let submit_button = document.createElement('button');
+    submit_button.textContent = 'Submit';
+    submit_button.addEventListener('click', () => {
+        let post = {
+            creator: user.displayName,
+            date: String(new Date()),
+            type: choice1.checked ? 'Blog' : choice2.checked ? 'Interview' : () => {throw err},
+            title: input.value,
+            description: description.value,
+        };
+        write_content(post);
+    });
+    container.appendChild(submit_button);
+}
+window.valid = valid;
+
+function write_content(post) {
+    let container = document.getElementById('submit-container');
+    container.innerHTML = '';
+
+    let title = document.createElement('h1');
+    title.textContent = 'Write your post';
+    container.appendChild(title);
+
+    let detail1 = document.createElement('p');
+    detail1.textContent = 'Write the post that will be submitted here.';
+    container.appendChild(detail1);
+
+    let detail2 = document.createElement('p');
+    detail2.textContent = "Do not include the title, or a section for the author's name. Proceed with the post from the first paragraph."
+    container.appendChild(detail2);
+
+    container.appendChild(webwrite.container);
+
+    let submit_button = document.createElement('button');
+    submit_button.textContent = 'Submit';
+    submit_button.addEventListener('click', () => {
+        post.content = webwrite.content();
+        submit(post);
+    });
+    container.appendChild(submit_button);
+}
 
 function submit(post) {
-  let submit_container = document.getElementById("submit-container");
+    console.log(post.content);
+    let content = post.content;
+    console.log(content);
+    console.log(post);
+    let user = auth.currentUser;
+    let submit_container = document.getElementById("submit-container");
     //let type = types[0].value == 'blog' ? 'blog' : 'interview'; // gets the type of post
     let posts_ref = ref(database, 'posts/' + post.type); // gets the reference to the folder with the type of post
     let new_post_ref = push(posts_ref); // both create a new post in each folder
     post.key = new_post_ref.key;
-    let new_user_post_ref = ref(database, 'users/' + uid + '/posts/' + post.key); 
+    let new_user_post_ref = ref(database, 'users/' + user.uid + '/posts/' + post.key); 
     let user_post = post;
     delete user_post['content'];
-    set(ref(database, 'content/' + post.key),{content: post.content});
+    set(ref(database, 'content/' + post.key),{content: content});
     set(new_post_ref, user_post).then(() => {
         set(new_user_post_ref, user_post).then(() => {
             let key = new_post_ref.key;
@@ -58,109 +163,4 @@ function submit(post) {
         });
     });
 
-
 }
-window.submit = submit;
-
-function display_step_2(post) {
-    let submit_container = document.getElementById("submit-container");
-    webwrite.enableImages();
-    submit_container.innerHTML = "<h1>Write your post</h1>";
-    submit_container.innerHTML += "<p>Write the post that will be submitted here.</p>";
-    submit_container.innerHTML += "<p>Do not include the title, or a section for the author's name. Proceed with the post from the first paragraph.";
-    submit_container.appendChild(webwrite.container);
-    submit_container.style.width = "75%";
-    let button = document.createElement('button');
-    button.textContent = "Submit";
-    button.addEventListener('click', () => {
-      post.content = webwrite.content();
-      console.log(post);
-      submit(post);
-    }); 
-    submit_container.appendChild(button);
-}
-
-function save_details() {
-  get(child(dbRef, '/users/' + uid)).then((snapshot) => {
-    let udata = snapshot.val();
-    let types = document.getElementsByName('format'); //
-    let type;
-    for(type of types) {
-      if(type.checked == true) {
-        type = type.value;
-        break;
-      }
-    }
-    let title = document.getElementById("title").value;
-    let description = document.getElementById("description").value;
-    let date = new Date();
-    date = String(date);
-    post = {
-        type: type,
-        creator_name: udata.name,
-        title: title,
-        description: description,
-        date: date,
-    };
-    display_step_2(post);
-  });
-}
-window.save_details = save_details;
-
-function valid() {
-    container.innerHTML = "";
-    container.innerHTML += "<p>Title of post</p>";
-    container.innerHTML += '<input id="title" type="text" name="title"></input>';
-    container.innerHTML += "<p>Write a brief description of your post.</p>";
-    container.innerHTML += '<textarea id="description"></textarea>';
-    container.innerHTML += "<p>Blog post or interview?</p>";
-    container.innerHTML += '<input id="format_1" type="radio" name="format" value="Blog">Blog</input>';
-    container.innerHTML += '<input id="format_2" type="radio" name="format" value="Interview">Interview</input>';
-    container.innerHTML += '<button id="save_details" onclick=save_details()>Submit</input>';
-}
-window.valid = valid;
-
-function invalid() {
-    console.log('click');
-}
-
-onAuthStateChanged(auth, (user) => {
-if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/auth.user
-    window.user = user;
-    uid = user.uid;
-    console.log(uid);
-    console.log(user);
-    set(ref(database, 'users/' + uid), {
-      name: user.displayName,
-      email: user.email,
-    });
-    document.getElementById("display_name").innerHTML = user.displayName;
-    // Basic text to submit a new post
-    document.getElementById("text-container").innerHTML = "<h1>Let's get started!</h1>";
-    document.getElementById("text-container").innerHTML += "<br><h2>Basic Info</h2>";
-    document.getElementById("text-container").innerHTML += "<br>";
-    document.getElementById("text-container").innerHTML += "<p>Let's confirm a few details about your post.</p>";
-    // Details
-    var name_text = document.createElement("p");
-    name_text.textContent = "Name: " + user.displayName;
-    container.appendChild(name_text);
-    var email_text = document.createElement("p");
-    email_text.textContent = "Email: " + user.email;
-    container.appendChild(email_text);
-    container.innerHTML += "<p>Is this information valid?</p>";
-    container.innerHTML += '<table><tr><td><button onclick="valid()">Yes</button></td><td><button onclick="invalid()">No</button></td></tr></table>';
-    container.innerHTML += "<p>Press no only if the name on your Google account is not your actual name or there are multiple authors.</p>";
-    container.innerHTML += "<br>";
-    container.innerHTML += "<p>If your Google account name is not your real name, you can add your real name to HerStory by going to the settings page.</p>";
-    
-    
-    // ...
-} else {
-  // User is signed out
-  console.log("User is signed out");
-  window.location.href = '/';
-  // ...
-}
-});
